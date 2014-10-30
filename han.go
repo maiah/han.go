fc43e8ea228eb85db64ea684f231ca75b474de34
maiah <jjmmacariola@gmail.com>
2014-10-31 00:02:34 +0800

    Initial routings for authentication

--- han.go
+++ han.go
@@ -2,14 +2,60 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
 )
 
 func main() {
+    // Setup route handlers
     http.HandleFunc("/", index)
+    http.HandleFunc("/login", login)
+    http.HandleFunc("/home", home)
+
+    // Start the server
+    log.Println("Listening on port 5000")
     http.ListenAndServe(":5000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "hello world")
+    path := r.URL.Path
+
+    if len(path) >= 7 {
+        path = r.URL.Path[0:7]
+    }
+
+    if path == "/public" {
+        public(w, r)
+    } else {
+        fmt.Fprintf(w, "welcome my han.go")
+    }
+}
+
+func public(w http.ResponseWriter, r *http.Request) {
+    file := r.URL.Path[1:]
+    http.ServeFile(w, r, file)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "GET" {
+        file := "pages/login.html"
+        http.ServeFile(w, r, file)
+
+    } else if r.Method == "POST" {
+        r.ParseForm()
+
+        username := r.PostForm["username"][0]
+        password := r.PostForm["password"][0]
+
+        if username == "maiah" && password == "abc123" {
+            http.Redirect(w, r, "/home", http.StatusFound)
+        } else {
+            fmt.Fprintf(w, "login now, %s", username)
+        }
+    }
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+    file := "pages/home.html"
+    http.ServeFile(w, r, file)
 }
