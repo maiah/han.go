9674c10e164ed71a60b3f98d57cf6987b995c9b1
jmacariola <jeremiah.macariola@optum.com>
2014-11-11 23:29:30 +0800

    added additional secure page

--- han.go
+++ han.go
@@ -19,6 +19,7 @@ func main() {
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/logout", logout)
 	http.HandleFunc("/home", home)
+	http.HandleFunc("/settings", settings)
 
 	// Start the server
 	log.Println("Listening on port 5000")
@@ -49,28 +50,28 @@ type Page struct {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("login")
+	fmt.Println("login")
 	if isAuthorized(r) {
-        fmt.Println("DEBUG 1")
+		fmt.Println("DEBUG 1")
 		http.Redirect(w, r, "/home", http.StatusFound)
 
 	} else {
-        fmt.Println("DEBUG 2")
+		fmt.Println("DEBUG 2")
 		loginPage := "pages/login.html"
 
 		if r.Method == "GET" {
-            fmt.Println("DEBUG 3")
+			fmt.Println("DEBUG 3")
 			t, _ := template.ParseFiles(loginPage)
 			t.Execute(w, nil)
 
 		} else if r.Method == "POST" {
-            fmt.Println("DEBUG 4")
+			fmt.Println("DEBUG 4")
 			if isAuthenticated(w, r) {
-                fmt.Println("DEBUG 5")
+				fmt.Println("DEBUG 5")
 				http.Redirect(w, r, "/home", http.StatusFound)
 
 			} else {
-                fmt.Println("DEBUG 6")
+				fmt.Println("DEBUG 6")
 				t, _ := template.ParseFiles(loginPage)
 				t.Execute(w, &Page{Message: "Invalid username/password"})
 			}
@@ -81,14 +82,14 @@ func login(w http.ResponseWriter, r *http.Request) {
 func logout(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "user-session")
 	session.Options = &sessions.Options{MaxAge: -1}
-    session.Values = nil
+	session.Values = nil
 	session.Save(r, w)
 
 	http.Redirect(w, r, "/login", http.StatusFound)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Home")
+	fmt.Println("Home")
 	if isAuthorized(r) {
 		file := "pages/home.html"
 		http.ServeFile(w, r, file)
@@ -98,9 +99,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func settings(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Settings")
+	if isAuthorized(r) {
+		file := "pages/settings.html"
+		http.ServeFile(w, r, file)
+
+	} else {
+		http.Redirect(w, r, "/login", http.StatusFound)
+	}
+}
+
 func isAuthorized(r *http.Request) bool {
 	session, _ := store.Get(r, "user-session")
-    fmt.Printf("isAuthorized? %s\n", session.Values["username"])
+	fmt.Printf("isAuthorized? %s\n", session.Values["username"])
 	return session.Values["username"] != nil
 }
 
